307a2aee41db530765e8df8b55965fee1b63c0d3
Denis Papathanasiou <denis@papathanasiou.org>
2020-03-08 13:57:29 -0400

    Added simpler json parser for the two hops required by the web client calls

--- src/main/java/org/papathanasiou/denis/WeatherAPIClient.java
+++ src/main/java/org/papathanasiou/denis/WeatherAPIClient.java
@@ -1,25 +1,103 @@
 package org.papathanasiou.denis;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
 
 public class WeatherAPIClient {
     private static final String baseURL = "https://api.weather.gov/";
     private static final String userAgent = "spring-webflux/0.0.1 +http://github.com/dpapathanasiou/spring-webflux";
+    private static final String EMPTY = "";
 
-    private WebClient client = WebClient.builder()
-            .baseUrl(baseURL)
-            .defaultHeader(HttpHeaders.USER_AGENT, userAgent)
-            .build();
+    private WebClient client;
 
-    public Mono<ClientResponse> lookupLatLong(String latitude, String longitude) {
+    public WeatherAPIClient() {
+        this.client = WebClient.builder()
+                .baseUrl(baseURL)
+                .defaultHeader(HttpHeaders.USER_AGENT, userAgent)
+                .build();
+    }
+
+    Function<String, String> parseForecastURL = json -> {
+        // within the json returned by the call to `/points/lat,long`,
+        // `"properties": "forecast":` contains the URL with the forecast data
+        JSONObject obj = new JSONObject(json);
+        if( obj.has("properties") ) {
+            JSONObject properties = obj.getJSONObject("properties");
+            if( properties.has("forecast") ) {
+                return properties.getString("forecast");
+            }
+        }
+        return EMPTY;
+    };
+
+    /**
+     * Use the `latitude` and `longitude` values to fetch the corresponding forecast URL as a string.
+     */
+    Mono<String> lookupLatLong(String latitude, String longitude) {
         return client
                 .get()
                 .uri(String.format("/points/%s,%s", latitude, longitude))
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange();  // resulting `"properties": "forecast":` contains the URL with the forecast data
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(json -> parseForecastURL.apply(json));
+    }
+
+    Function<String, String> parseForecastResponse = json -> {
+        // within the json returned by the call to `/forecast`,
+        // `"properties": "periods":` contains a list of forecasts,
+        // starting with `"name": "Today"` as the current one we want
+        JSONObject obj = new JSONObject(json);
+        if( obj.has("properties") ) {
+            JSONObject properties = obj.getJSONObject("properties");
+            if( properties.has("periods") ) {
+                return StreamSupport.stream(properties.getJSONArray("periods").spliterator(), false)
+                        .map(JSONObject.class::cast)
+                        .filter(x -> x.has("name"))
+                        .filter(x -> x.getString("name").equals("Today"))
+                        .filter(x -> x.has("detailedForecast"))
+                        .findFirst()
+                        .map(x -> x.getString("detailedForecast"))
+                        .orElse(EMPTY);
+            }
+        }
+        return EMPTY;
+    };
+
+    Function<String, String> parseForecastURLPath = url -> {
+        // split the string `https://api.weather.gov/gridpoints/OKX/32,37/forecast`
+        // and remove the base url, returning just the relevant path
+        try {
+            return new URL(url).toURI().getPath();
+        } catch (MalformedURLException | URISyntaxException e) {
+            System.err.println(e.getMessage());
+        }
+        return EMPTY;
+    };
+
+    Mono<String> lookupForecast(String path) {
+        return client
+                .get()
+                .uri(path)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(json -> parseForecastResponse.apply(json));
+    }
+
+    public Mono<String> getForecast(String latitude, String longitude) {
+        return lookupLatLong(latitude, longitude)
+                .map(forecastURL -> parseForecastURLPath.apply(forecastURL))
+                .filter(forecastPath -> ! forecastPath.equals(EMPTY))
+                .flatMap(this::lookupForecast);
     }
 }
--- src/main/java/org/papathanasiou/denis/WeatherHandler.java
+++ src/main/java/org/papathanasiou/denis/WeatherHandler.java
@@ -2,22 +2,23 @@ package org.papathanasiou.denis;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
 public class WeatherHandler {
-    private WeatherAPIClient client = new WeatherAPIClient();
+    private WeatherAPIClient client;
+
+    public WeatherHandler() {
+        this.client = new WeatherAPIClient();
+    }
 
     public Mono<ServerResponse> getForecast(ServerRequest request) {
+        Mono<String> forecast = client.getForecast(request.pathVariable("lat"), request.pathVariable("long"));
         return ServerResponse
                 .ok()
                 .contentType(MediaType.TEXT_PLAIN)
-                //.body(BodyInserters.fromValue(client.lookupLatLong(request.pathVariable("lat"), request.pathVariable("long")).bodyToMono(String.class)));
-                .body(BodyInserters.fromValue(String.format("Hello, lat %s, long %s!",
-                       request.pathVariable("lat"),
-                       request.pathVariable("long"))));
+                .body(forecast, String.class);
     }
 }
